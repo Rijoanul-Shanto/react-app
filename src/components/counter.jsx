dfdbae36cdfb40bac85c81b1c0ef710b3a583e58
Rijoanul Hasan Shanto <rijoanul.shanto@gmail.com>
2021-02-18 02:50:11 +0600

    [ADD] - increment and list functionality added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,15 +3,44 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    tags: ["tag1", "tag2", "tag3"],
   };
+
+  constructor() {
+    super();
+    this.handleIncremet = this.handleIncremet.bind(this);
+  }
 
   render() {
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
-        <button className="btn btn-primary btn-sm">Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncremet}
+          className="btn btn-primary btn-sm"
+        >
+          Increment
+        </button>
+        <ul>{this.renderTags()}</ul>
       </div>
     );
+  }
+
+  handleIncremet() {
+    this.setState({ count: ++this.state.count });
+    console.log(this.state.count);
+  }
+
+  renderTags() {
+    if (!this.state.tags.length) return <p>There are no tags!</p>;
+
+    return this.state.tags.map((tag) => <li key={tag}>{tag}</li>);
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+
+    return (classes += 0 === this.state.count ? "warning" : "danger");
   }
 
   formatCount() {
